37a3e980ed7c909c8c84c79fa0cc4df9e4e71f24
Dashersd <helmandacuma5@gmail.com>
2026-01-04 08:33:44 +0800

    Add cache-busting parameters to CSS files to force browser reload - Added version parameter to admin_collection.css and admin_login.css - This will force browsers to reload CSS after deployment

--- admin_collection.php
+++ admin_collection.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>D'MARSIANS Taekwondo System - Admin Collection</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="Styles/admin_collection.css">
+    <link rel="stylesheet" href="Styles/admin_collection.css?v=20250127">
     <link rel="stylesheet" href="Styles/sidebar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
--- admin_login.php
+++ admin_login.php
@@ -8,7 +8,7 @@ ini_set('display_errors', 1);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Super Admin Login - D'MARSIANS Taekwondo System</title>
-    <link rel="stylesheet" href="Styles/admin_login.css">
+    <link rel="stylesheet" href="Styles/admin_login.css?v=20250127">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Inter:wght@300;400;500;600;700;800;900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Source+Serif+Pro:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
