23de2c773bafe289c930a71a4d358c95fea46508
Dashersd <helmandacuma5@gmail.com>
2026-01-05 11:40:06 +0800

    Add favicon link to eliminate favicon.ico 404 error
    - Added favicon link using Logo2.png in the HTML head
    - Eliminates the harmless but annoying favicon.ico 404 error in console
    - Uses existing logo image as favicon

--- webpage.php
+++ webpage.php
@@ -20,6 +20,7 @@ if (empty($heroVideoUrl)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>D'MARSIANS TAEKWONDO GYM</title>
+    <link rel="icon" type="image/png" href="Picture/Logo2.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="Styles/webpage.css">
